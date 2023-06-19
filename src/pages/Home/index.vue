76631bad7ba3017309b2b534617658477088350d
HzhCjf <h2240915370@gmail.com>
2023-06-19 15:54:57 +0800

    add:Home的静态组件

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,13 +1,37 @@
 <template>
-    <div>Home</div>
+  <div>
+    <TypeNav/>
+    <List/>
+    <Recommend/>
+    <Rank/>
+    <Like/>
+    <Floor/>
+    <Floor/>
+    <Brand/>
+  </div>
 </template>
 
 <script>
+import TypeNav from "@/pages/Home/components/TypeNav";
+import List from "@/pages/Home/components/List";
+import Recommend from "@/pages/Home/components/Recommend";
+import Rank from "@/pages/Home/components/Rank";
+import Like from "@/pages/Home/components/Like";
+import Floor from "@/pages/Home/components/Floor";
+import Brand from "@/pages/Home/components/Brand";
 export default {
-    name:'Home'
-}
+  name: "Home",
+  components: {
+    TypeNav,
+    List,
+    Recommend,
+    Rank,
+    Like,
+    Floor,
+    Brand,
+  },
+};
 </script>
 
 <style>
-
 </style>
